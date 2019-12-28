5ad6b2bd396893cd7197689f8f15a53ccd0245ba
zaenury5868 <zaenury5868@gmail.com>
2019-12-28 20:30:03 +0700

    add answer number 3

---
+++ 3.js
@@ -0,0 +1,15 @@
+function printWords(words){
+    const vocals = ['a', 'i', 'u', 'e', 'o'];
+    var filtered_words = [];
+
+    for (let i = 0; i < words.length; i++) {
+        if (vocals.includes(words.charAt(i))){
+            console.log(words.charAt(i))
+        }
+    }
+    for (let i = 0; i < words.length; i++) {
+        if (!vocals.includes(words.charAt(i))){
+            console.log(words.charAt(i))
+        }
+    }
+}
